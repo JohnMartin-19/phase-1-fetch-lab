b936fbbb188dccbd8fd5414b5197253fdcd73c3d
John Mburu <johnnkonge2020@gmail.com>
2023-09-30 11:07:37 +0300

    not done

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
-}
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => books = (json));
+  
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+  if(fetch === true){
+    
+
+    function renderBooks(books) {
+      const main = document.querySelector('main');
+      books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+      });
+    }
+     renderBooks(books)
+  }
+};
+
+fetchBooks()
+///
+
+//*
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
